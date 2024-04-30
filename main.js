17dc08d99f9ddcf651d3ca8e7b543871b514211c
Franklin Lorran <lorranfranklin.dev@gmail.com>
2024-04-30 07:43:44 -0300

    first commit, diferença entre o uso de js puro e de jQuery

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  //usando jQuery para selecionar e adicionar eventos
+  $('header button').click(function() {
+    alert('clicou no botão');
+  });
+  //usando apenas javaScript para selecionar e adicionar eventos
+  //form.addEventListener('submit', function(event) {
+   // event.preventDefault();
+   // alert('clicou no botão');
+  //})
+  
+  //usando jQuery para selecionar e adicionar eventos
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    alert('clicou no botão');
+  });
+});
